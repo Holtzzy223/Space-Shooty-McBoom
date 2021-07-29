07216284e33f88c5d991e0290f7604e8e2bc581a
Holtzzy223 <70851222+Holtzzy223@users.noreply.github.com>
2021-07-29 18:20:46 -0400

    barrel rolls

--- Space Shooty McBoom/Assets/PlayerController.cs
+++ Space Shooty McBoom/Assets/PlayerController.cs
@@ -12,25 +12,62 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float controlPitchFactor = -10f;
     [SerializeField] float positionYawFactor = 2f;
     [SerializeField] float controlRollFactor = -20f;
-    public CinemachineDollyCart cart;
-    float xThrow, yThrow;
 
+    private bool isShipRolling = false;
+  
+    public CinemachineDollyCart cart;
+    [SerializeField] float xThrow, yThrow;
+    [SerializeField] float rollDirection;
+    [SerializeField] float rollFactor = 0f;
+    [SerializeField] float maxRollFactor = 360f;
+    [SerializeField] float smoothTime = 10f;
+    [SerializeField] float minRollFactor = 0f;
+    [SerializeField] float barrelSmoothTime = 6f;
     void Update()
     {
         ProcessTranslation();
         ProcessRotation();
+        if(Input.GetButtonDown("Fire1"))
+        {
+            isShipRolling = true;
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            rollFactor = Mathf.Lerp(0, minRollFactor, smoothTime * Time.deltaTime);
+            isShipRolling = false;
+
+        }
+
     }
 
     void ProcessRotation()
     {
-        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
-        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        if (!isShipRolling)
+        {
+            float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+            float pitchDueToControlThrow = yThrow * controlPitchFactor;
 
-        float pitch = pitchDueToPosition + pitchDueToControlThrow;
-        float yaw = transform.localPosition.x * positionYawFactor;
-        float roll = xThrow * controlRollFactor;
+            float pitch = pitchDueToPosition + pitchDueToControlThrow;
+            float yaw = transform.localPosition.x * positionYawFactor;
 
-        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+            float roll = xThrow * controlRollFactor;
+            Quaternion targetRotation = Quaternion.Euler(pitch, yaw, roll);
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, 0.1f);
+        }else
+        if (isShipRolling)
+        {
+            rollDirection = Input.GetAxis("Roll");
+            rollFactor = Mathf.Lerp(rollFactor, maxRollFactor, smoothTime * Time.deltaTime);
+            float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+            float pitchDueToControlThrow = yThrow * controlPitchFactor;
+            float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+            float yaw = transform.localPosition.x * positionYawFactor;
+
+            float roll = rollDirection * rollFactor;
+            Quaternion targetRotation = Quaternion.Euler(pitch, yaw, roll);
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, 0.1f);
+        }
     }
 
     void ProcessTranslation()
@@ -45,8 +82,8 @@ public class PlayerController : MonoBehaviour
         float yOffset = yThrow * Time.deltaTime * controlSpeed;
         float rawYPos = transform.localPosition.y + yOffset;
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
-
-        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+        Vector3 targetTranslation = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetTranslation, 0.8f);
     }
 }
 
