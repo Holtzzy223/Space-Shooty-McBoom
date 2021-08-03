73a9825363be3995d7fef28c4a4cd3c60671911c
Holtzzy223 <70851222+Holtzzy223@users.noreply.github.com>
2021-08-03 15:01:54 -0400

    boids

--- Space Shooty McBoom/Assets/Scripts/PlayerStats.cs
+++ Space Shooty McBoom/Assets/Scripts/PlayerStats.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
+    public float health = 100;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,21 @@ public class PlayerStats : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (health < 1)
+        {
+            Destroy(this);
+        }
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log($"{name}I'm hit! by {other.gameObject.name}");
+        health -= 10f;
+       
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        health -= 5f;
         
     }
+
 }
